dd2b886d742442a18130767b6d016cbfde8fefa0
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-21 16:17:25 -0700

    added components and browserrouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Jokes from './View/Jokes/Jokes';
+import Header from './Components/Header/Header';
+import Breakfast from './View/Breakfast/Breakfast';
+import Families from './View/Families/Families';
+import Home from './View/Main/Home';
+import Movies from './View/Movies/Movies';
 
 function App() {
   return (
-    <div className="App">
-      <Jokes />
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path={'/'}>
+          <Home />
+        </Route>
+        <Route exact path={'/breakfast'}>
+          <Breakfast />
+        </Route>
+        <Route exact path={'/families'}>
+          <Families />
+        </Route>
+        <Route exact path={'/movies'}>
+          <Movies />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/services/fetchJokes.js
+++
@@ -1,6 +0,0 @@
-import { checkError, client } from './client';
-
-export async function getJokes() {
-  const resp = await client.from('jokes').select('*, genre_id (*)');
-  return checkError(resp);
-}
